8c0583daeddb28c6e0b3f309208e729302f2f317
Geunseok Byeon <byeongeunseok@gmail.com>
2023-06-23 19:40:10 +0900

    add a time check log

--- modules/number_shuffle.py
+++ modules/number_shuffle.py
@@ -1,7 +1,11 @@
 import random
+import time
 
 
 def number_shuffle(amt, min, max, shuffle):
+    # Time start
+    start_time = time.time()
+
     numberList = list(range(min, max+1))
 
     for sfl in range(shuffle):
@@ -9,4 +13,8 @@ def number_shuffle(amt, min, max, shuffle):
 
     print(numberList[:amt])
 
+    # Time End
+    end_time = time.time()
+
+    print(end_time - start_time)
     return 0
