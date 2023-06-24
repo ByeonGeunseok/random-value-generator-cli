877d5ac2491492305a85fa12786730a94d1b1755
Geunseok Byeon <byeongeunseok@gmail.com>
2023-06-24 17:49:28 +0900

    modify random number logic to use zero

--- modules/random_numbers.py
+++ modules/random_numbers.py
@@ -1,8 +1,12 @@
 import random
 
+
 def create_random_number():
     random_number_string = ""
-    for _ in range(random.randint(4, 9)):
-        random_number_string += str(random.randint(1, 9))
+    for _ in range(random.randint(4, 8)):
+        if not random_number_string:
+            random_number_string += str(random.randint(1, 9))
+        else:
+            random_number_string += str(random.randint(0, 9))
 
     return random_number_string
