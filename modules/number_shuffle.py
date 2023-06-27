09f8de814aaf2c56d6d783e4a4156b70369675a2
Geunseok Byeon <byeongeunseok@gmail.com>
2023-06-27 22:15:35 +0900

    add a progress status

--- modules/number_shuffle.py
+++ modules/number_shuffle.py
@@ -1,12 +1,15 @@
 import random
 import time
+from rich.console import Console
 
 
 def number_shuffle(amt, min, max, shuffle):
+    console = Console()
     numberList = list(range(min, max+1))
 
-    for sfl in range(shuffle):
-        random.shuffle(numberList)
+    with console.status("[bold green]Working on tasks...") as status:
+        for _ in range(shuffle):
+            random.shuffle(numberList)
 
     print(numberList[:amt])
 
